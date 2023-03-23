fa2294c79cf65e6936457ed98b7833490ba4a5eb
Nataliia D <113416916+SambykaLight@users.noreply.github.com>
2023-03-23 13:37:15 +0100

    final

--- src/JS/createMarkup.js
+++ src/JS/createMarkup.js
@@ -1,24 +1,23 @@
 export function createMarkupCountryList(data) {
   const markup = data
     .map(({ name, flags }) => {
-      return `<li><img src="${flags.svg}" alt="${name.official}" width="60" height="40">${name.official}</li>`;
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="20" height="20">${name.official}</li>`;
     })
     .join('');
+  return markup;
 }
 
 export function createMarkupCountryInfo(data) {
-    const markup = data.map(({ name, capital, population, flags, language }) => {
-        return `<div class = "card">
-    <div class = "card-img">
+  const markup = data
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<div class = "card">
         <p class ="card-name">Name: ${name.official}</p>
-<img src="${flags.svg}" alt="${name.official}">
-    </div>
-    <ul class ="card-info"> 
+        <img src="${flags.svg}" alt="${name.official}"  width = "80">
         <p class="card-item">capital: ${capital}</p>
-        <p class="card-item">population:${population}</p>
-        <p class="card-item">languages: ${(language).join(', ')}</p>
-    </ul>
+        <p class="card-item">population: ${population}</p>
+        <p class="card-item">languages: ${Object.values(languages)}</p>
 </div>`;
-    }).join('');
+    })
+    .join('');
+  return markup;
 }
-
